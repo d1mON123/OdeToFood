7340ef37a5097107df39d12f05034af0c7b5fb5f
Dima Shmundiak <dimashmund@gmail.com>
2017-08-02 14:35:25 +0300

    Overview of ASP.NET Core project Created service

---
+++ OdeToFood/Greeter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public class Greeter:IGreeter
+    {
+        private readonly string _greeting;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _greeting = configuration["Greeting"];
+        }
+
+        public string GetGreeting()
+        {
+            return _greeting;
+        }
+    }
+}
