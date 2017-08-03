f07174cd11282df87453433337c4c08dad0428f2
Dima Shmundiak <dimashmund@gmail.com>
2017-08-03 14:42:57 +0300

    Added creating of new restaurant

---
+++ OdeToFood/Entities/Restaurant.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.Entities
+{
+    public enum CuisineType
+    {
+        None,
+        Italian,
+        French,
+        Japanese,
+        American
+    }
+
+    public class Restaurant
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "RestaurantName")]
+        [Required, MaxLength(80)]
+        public string Name { get; set; }
+
+        public CuisineType Cuisine { get; set; }
+    }
+}
