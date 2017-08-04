fa31544694a080f5cb80bd342339e03ab9ee9c31
Dima Shmundiak <dimashmund@gmail.com>
2017-08-04 16:06:50 +0300

    Added styling

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -50,9 +50,9 @@ namespace OdeToFood
                 });
             }
             app.UseFileServer();
+            app.UseNodeModules(env.ContentRootPath);
             app.UseIdentity();
             app.UseMvc(ConfigureRoutes);
-            app.Run(ctx => ctx.Response.WriteAsync("Not found"));
         }
 
         private static void ConfigureRoutes(IRouteBuilder routeBuilder)
